41dbae1d8ac143be9cf1580c36ae70cbba6fe32e
IureDiniz <139821101+IureDiniz@users.noreply.github.com>
2024-11-19 19:36:12 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,43 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-    int lin, col;
-    scanf("%d", &lin);
-    scanf("%d", &col);
+int main() {
+	int lin, col;
+	scanf("%d", &lin);
+	scanf("%d", &col);
 
-    int **matriz = (int**) malloc(lin * sizeof(int*));
-    if(lin == 0 || col == 0){
-        matriz[0] = NULL;
-    }
-    else{
-        for(int i = 0; i < lin; i++){
-            matriz[i] = (int*) malloc(col * sizeof(int)); 
-        }
-    }
+	int **matriz = (int**) malloc(lin * sizeof(int*));
+	if(lin == 0 || col == 0) {
+		matriz[0] = NULL;
+	}
+	else {
+		for(int i = 0; i < lin; i++) {
+			matriz[i] = (int*) malloc(col * sizeof(int));
+		}
+	}
 
-    if(matriz[0] != NULL){
-        for(int i = 0; i < lin; i++){
-            for(int j = 0; j < col; j++){
-                int aux = 0;
-                scanf(" %d", &aux);
-                matriz[i][j] = aux; 
-            }
-        }
+	if(matriz[0] != NULL) {
+		for(int i = 0; i < lin; i++) {
+			for(int j = 0; j < col; j++) {
+				int aux = 0;
+				scanf(" %d", &aux);
+				matriz[i][j] = aux;
+			}
+		}
 
-        for(int i = 0; i < lin*col; i++){
-                for(int j = 0; j < col; j++){
-                printf("%d ", matriz[i][j]);
-            }
-         printf("\n");
-        }
-    }else{
-        printf("[matriz vazia]\n");
-    }
+		for(int i = 0; i < lin*col; i++) {
+			for(int j = 0; j < col; j++) {
+				if(j == col-1) {
+					printf("%d", matriz[i][j]);
+				}else{
+					printf("%d ", matriz[i][j]);
+				}
+			}
+			printf("\n");
+		}
+	} else {
+		printf("[matriz vazia]\n");
+	}
 
-    for(int i = 0; i < lin; i++){
-        free(matriz[i]);
-    }
-    free(matriz);
-    return 0;
+	for(int i = 0; i < lin; i++) {
+		free(matriz[i]);
+	}
+	free(matriz);
+	return 0;
 }
